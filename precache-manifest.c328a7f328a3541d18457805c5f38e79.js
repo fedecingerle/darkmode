732c5c6e87dadd6f69e90319a6f3ec76e19d861e
fedecingerle <fedecingerle@gmail.com>
2020-09-17 15:52:58 -0300

    Updates

---
+++ precache-manifest.c328a7f328a3541d18457805c5f38e79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ee561a0de8eaa3f918f06777b20eac",
+    "url": "/darkmode/index.html"
+  },
+  {
+    "revision": "8d1cc151f301cb2eb4a7",
+    "url": "/darkmode/static/css/main.23af2d43.chunk.css"
+  },
+  {
+    "revision": "824bd0c313442b154f07",
+    "url": "/darkmode/static/js/2.b7e0bc99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/darkmode/static/js/2.b7e0bc99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d1cc151f301cb2eb4a7",
+    "url": "/darkmode/static/js/main.b7a21e26.chunk.js"
+  },
+  {
+    "revision": "37c68d41a94fb33cbf09",
+    "url": "/darkmode/static/js/runtime-main.b2f89632.js"
+  }
+]);
